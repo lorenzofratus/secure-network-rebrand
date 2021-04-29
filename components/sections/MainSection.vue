8719159efa9acd77f43cec4dc7563fd678c2dd22
Cristian C. Spagnuolo <cristian.c.spagnuolo@gmail.com>
2021-04-29 19:16:47 +0200

    definition of the the main section

---
+++ components/sections/MainSection.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="header">
+		<div class="content">
+			<h1>{{ title }}</h1>
+			<h3>{{ subtitle }}</h3>
+			<div class="buttons">
+				<nuxt-link
+					v-for="(button, index) in buttons"
+					:key="'main-section-button-' + index + '-button.wrapper'"
+					:class="'button ' + button.class"
+					:to="button.path"
+				>
+					{{ button.text }}
+				</nuxt-link>
+			</div>
+		</div>
+		<component :is="img.src" :class="img.class" />
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		title: {
+			type: String,
+			required: true,
+		},
+		subtitle: {
+			type: String,
+			required: false,
+			default: '',
+		},
+		buttons: {
+			type: Array,
+			required: true,
+		},
+		img: {
+			required: true,
+			type: Object,
+		},
+	},
+}
+</script>
+
+<style scoped>
+.header {
+	min-height: 50vh;
+	max-width: var(--page);
+	margin: 4em auto;
+	padding: 0 var(--padding);
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	flex-wrap: wrap-reverse;
+}
+.header > * {
+	flex: 0 1 auto;
+	max-width: var(--half-page);
+	margin: 0 auto;
+}
+.header .buttons {
+	justify-content: start;
+}
+.header .cover {
+	display: block;
+	padding: 5%;
+	box-sizing: border-box;
+	height: auto;
+	width: 100%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,44 @@
 <template>
-	<div class="container"></div>
+	<div class="container">
+		<main-section
+			:buttons="buttons"
+			:img="img"
+			:title="title"
+			:subtitle="subtitle"
+		></main-section>
+	</div>
 </template>
 
 <script>
+import MainSection from '~/components/sections/MainSection.vue'
+import Cover from '~/components/images/Cover.vue'
 export default {
+	components: {
+		MainSection,
+	},
 	layout: 'default',
+	data() {
+		return {
+			title: 'Secure Network',
+			subtitle: 'Your Protection, Our Mission',
+			buttons: [
+				{
+					class: 'primary',
+					text: "Let's begin",
+					path: '/',
+				},
+				{
+					class: 'secondary',
+					text: 'Contact us',
+					path: '/contacts',
+				},
+			],
+			img: {
+				src: Cover,
+				class: 'cover',
+			},
+		}
+	},
 }
 </script>
 
