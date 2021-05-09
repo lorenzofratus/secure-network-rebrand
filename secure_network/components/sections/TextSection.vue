9feb0a66b5fa330dff299ef51f1ecf4383281987
simoneorlando21 <simone.orlando@mail.polimi.it>
2021-05-09 11:34:14 +0200

    Created resource page

---
+++ secure_network/components/sections/TextSection.vue
@@ -0,0 +1,33 @@
+<template>
+	<section class="text-section">
+		<div v-if="paragraphs" class="paragraphs content">
+			<p
+				v-for="(p, index) in paragraphs"
+				:key="'paragraph-' + index + '-' + wrapper"
+				:class="{ centered: centered }"
+			>
+				{{ p }}
+			</p>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	props: {
+		paragraphs: {
+			type: Array,
+			required: false,
+			default() {
+				return []
+			},
+		},
+	},
+}
+</script>
+
+<style>
+p.centered {
+	text-align: center;
+}
+</style>
