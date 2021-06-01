d554809f800f9734066b76af307c571a81b834dd
Cristian <cristian.c.spagnuolo@gmail.com>
2021-06-01 10:59:03 +0200

    Chatbot update

--- secure_network/mixins/mmcc-mixin.js
+++ secure_network/mixins/mmcc-mixin.js
@@ -46,54 +46,42 @@ export default {
 			const self = this
 			console.log(message)
 			if (message.utterance) {
-				// The bot prints twice the start message when restarts from the beginning
-				// To avoid the visualization of such a duplicated message, when it is detected
-				// it is sent a dummy payload to the backend to skip this message and receive directly
-				// the second one
-				if (
-					message.utterance.includes('Exploit') &&
-					!message.utterance.includes('tour') &&
-					self.$store.state.chat.messages.length
-				) {
-					self.skip(WebSocketEventBus)
-				} else {
-					// Adding 1 second timeout before adding the first message to the chat and then 0.55 seconds for each subsequent message
-					// This makes the chatbot less "aggressive" towards the user
-					const utterances = message.utterance.split('\n')
-					const speed = 550
-					let timeout = 1000
-					for (let utterance of utterances) {
-						// Utterance replacement if you are already on the given page, to give a feedback to the user
-						// since router.push() does not refresh the page if you are already there.
-						if (
-							self.$router.history.current.fullPath ===
-								message.payload.guide &&
-							utterance.includes('This is our')
-						) {
-							utterance = utterance.replace(
-								'This is',
-								'You are already on'
-							)
-						}
-						if (
-							self.$router.history.current.fullPath ===
-								message.payload.guide &&
-							utterance.includes('Let me take you to')
-						) {
-							utterance = utterance.replace(
-								'Let me take you to',
-								'You are already on'
-							)
-						}
-
-						setTimeout(() => {
-							self.$store.commit('chat/addMessage', {
-								sender: true,
-								content: utterance,
-							})
-						}, timeout)
-						timeout += speed
+				// Adding 1 second timeout before adding the first message to the chat and then 0.55 seconds for each subsequent message
+				// This makes the chatbot less "aggressive" towards the user
+				const utterances = message.utterance.split('\n')
+				const speed = 550
+				let timeout = 1000
+				for (let utterance of utterances) {
+					// Utterance replacement if you are already on the given page, to give a feedback to the user
+					// since router.push() does not refresh the page if you are already there.
+					if (
+						self.$router.history.current.fullPath ===
+							message.payload.guide &&
+						utterance.includes('This is our')
+					) {
+						utterance = utterance.replace(
+							'This is',
+							'You are already on'
+						)
 					}
+					if (
+						self.$router.history.current.fullPath ===
+							message.payload.guide &&
+						utterance.includes('Let me take you to')
+					) {
+						utterance = utterance.replace(
+							'Let me take you to',
+							'You are already on'
+						)
+					}
+
+					setTimeout(() => {
+						self.$store.commit('chat/addMessage', {
+							sender: true,
+							content: utterance,
+						})
+					}, timeout)
+					timeout += speed
 				}
 			}
 			setTimeout(() => {
